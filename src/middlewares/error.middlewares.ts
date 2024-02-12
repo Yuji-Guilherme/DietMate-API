fdd8017e9910681f0fdcd2261b802a84a776143b
Yuji-Guilherme <guilherme.nagasaki@outlook.com>
2024-02-12 13:59:04 -0300

    fix: add next fn no error middleware

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -4,11 +4,12 @@ import type { ApiError } from '../helpers/api-errors';
 const errorMiddleware = (
   error: Partial<ApiError>,
   req: Express.Request,
-  res: Express.Response
+  res: Express.Response,
+  next: Express.NextFunction
 ) => {
   const status = error.status ?? 500;
   const message = error.message ?? 'Internal Server Error';
-  return res.status(status).send({ message });
+  return res.status(status).json({ message });
 };
 
 export { errorMiddleware };
